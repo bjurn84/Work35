de506b61d62640e671e351e2df879d10616e1172
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2022-11-27 08:24:40 +0400

    Work35

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int [] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-1000, 1000);
+    }
+    return arr;
+}
+
+Console.Write("Введите число массива ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 & array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел от 10 до 99 это {count}");
